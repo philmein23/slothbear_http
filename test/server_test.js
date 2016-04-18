e33cbef4af0589aed6579967c78416538928ba42
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-17 21:55:10 -0700

    removed redundant tests

--- test/server_test.js
+++ test/server_test.js
@@ -8,26 +8,6 @@ const request = chai.request;
 
 const app = require(__dirname + '/../index.js');
 
-describe('testing server creation', () => {
-  before(() => {
-    app.router.get('/bear', (req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('GET method set up for /bear route');
-      res.end();
-    });
-    app.server(6265);
-  });
-  it('should respond to GET request to /bear on port 6265', (done) => {
-    request('localhost:6265')
-      .get('/bear')
-      .end((req, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.contain('GET method set up for /bear route');
-        done();
-      });
-  });
-});
-
 describe('testing routes', () => {
   before(() => {
       app.router.get('/sloth', (req, res) => {
@@ -59,10 +39,10 @@ describe('testing routes', () => {
         res.write('DELETE method hit on /sloth');
         res.end();
       });
-      app.server(6172);
+      app.server(6265);
   });
   it('Successfully handle GET request', (done) => {
-    request('localhost:6172')
+    request('localhost:6265')
       .get('/sloth')
       .end((req, res) => {
         expect(res).to.have.status(200);
@@ -71,7 +51,7 @@ describe('testing routes', () => {
       });
   });
   it('Successfully handle POST request', (done) => {
-    request('localhost:6172')
+    request('localhost:6265')
       .post('/sloth')
       .send({ 'Name': 'Fur' })
       .end((req, res) => {
@@ -81,7 +61,7 @@ describe('testing routes', () => {
       });
   });
   it('Successfully handle PUT request', (done) => {
-    request('localhost:6172')
+    request('localhost:6265')
       .put('/sloth')
       .send({ 'Name': 'Ball' })
       .end((req, res) => {
@@ -91,7 +71,7 @@ describe('testing routes', () => {
       });
   });
   it('Successfully handle PATCH request', (done) => {
-    request('localhost:6172')
+    request('localhost:6265')
       .patch('/sloth')
       .end((req, res) => {
         expect(res).to.have.status(200);
@@ -100,7 +80,7 @@ describe('testing routes', () => {
       });
   });
   it('Successfully handle DELETE request', (done) => {
-    request('localhost:6172')
+    request('localhost:6265')
       .delete('/sloth')
       .end((req, res) => {
         expect(res).to.have.status(200);
